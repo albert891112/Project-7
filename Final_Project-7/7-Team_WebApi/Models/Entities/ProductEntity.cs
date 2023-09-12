e2d0467cc62cfaeed45e13dddce600cfb901f1da
albert891112 <albert96002891112@gmail.com>
2023-09-12 14:30:32 +0800

    modify ProductRepository

--- Final_Project-7/7-Team_WebApi/Models/Entities/ProductEntity.cs
+++ Final_Project-7/7-Team_WebApi/Models/Entities/ProductEntity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace _7_Team_WebApi.Models.Entities
 {
     public class ProductEntity
     {
+        public int Id { get; set; }
+        public List<CategoryEntity> Categories { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public StockEntity Stock { get; set; }
+        public bool Enable { get; set; }
     }
 }
--- Final_Project-7/7-Team_WebApi/Repositories/ProductRepository.cs
+++ Final_Project-7/7-Team_WebApi/Repositories/ProductRepository.cs
@@ -4,21 +4,87 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using _7_Team_WebApi.Models.Entities;
+using System.Data.SqlClient;
+using Antlr.Runtime.Misc;
+using Dapper;
+using System.Data;
 
 namespace _7_Team_WebApi.Repositories
 {
     public class ProductRepository : IRepository<ProductEntity>
     {
 
+        SqlDb connection = new SqlDb();
+
+        /// <summary>
+        /// Get a product by ide
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public ProductEntity Get(int id)
         {
-            throw new NotImplementedException();
+            string sql = "SELECT p.* , C.* , S.* " +
+				"FROM Products as P " +
+				"INNER JOIN " +
+				"(Categroysies_Products as CP INNER JOIN Categories as C ON CP.CategoryId = C.Id)  " +
+				"ON P.id = CP.ProductId " +
+				"INNER JOIN Stock as S ON S.Id = P.StockId " +
+				"ORDER BY P.id";
+
+            object obj = new
+            {
+                Id = id
+            };
+
+            ProductEntity entity = this.connection.Get<ProductEntity>(sql, "defualt" , obj);
+
+            return entity;
         }
 
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
         public List<ProductEntity> GetAll()
         {
-            throw new NotImplementedException();
+            string sql = "SELECT p.* , C.* , S.* " +
+               "FROM Products as P " +
+               "INNER JOIN " +
+               "(Categroysies_Products as CP INNER JOIN Categories as C ON CP.CategoryId = C.Id)  " +
+               "ON P.id = CP.ProductId " +
+               "INNER JOIN Stock as S ON S.Id = P.StockId " +
+               "ORDER BY P.id";
+
+            //Delegate for get all products
+            Func<SqlConnection, string, List<ProductEntity>> func = (conn, s) =>
+            {
+                Dictionary<int, ProductEntity> products = new Dictionary<int, ProductEntity>();
+
+                conn.Query<ProductEntity, CategoryEntity, StockEntity, ProductEntity>(s, (product, category, stock) =>
+                {
+                    if (!products.TryGetValue(product.Id, out ProductEntity p))
+                    {
+                        product.Stock = stock;
+                        product.Categories = new List<CategoryEntity>();
+                        product.Categories.Add(category);
+                        products.Add(product.Id, product);
+                    }
+                    else
+                    {
+                        p.Categories.Add(category);
+                    }
+                    return p;
+
+                }, commandType: CommandType.StoredProcedure);
+
+                return products.Values.ToList();
+            };
+
+            //Get all products
+            List<ProductEntity> entities = this.connection.GetAll<ProductEntity>(sql, "defualt" , func);
+
+            return entities;
         }
 
 
