d172fe19a98722abb07ad5e23c1a711945e4aea7
DESKTOP-5GL18A3\River <medamayaki09@gmail.com>
2023-09-14 11:22:19 +0800

    [feature] Add RegisterVm

--- Final_Project-7_Client/Team-7_WebApi-Client/Models/Entities/MemberEntity.cs
+++ Final_Project-7_Client/Team-7_WebApi-Client/Models/Entities/MemberEntity.cs
@@ -11,25 +11,14 @@ namespace Team_7_WebApi_Client.Models.Entities
 	{
 		public int Id { get; set; }
 
-		[Display(Name = "帳號")]
-		[Required]
 		public string Account { get; set; }
 
-		[Display(Name = "密碼")]
-		[Required]
 		public string Password { get; set; }
 
-		[Display(Name = "電子郵件")]
-		[Required]
-		[EmailAddress]
 		public string Email { get; set; }
 
-		[Display(Name = "名")]
-		[Required]
 		public string FirstName { get; set; }
 
-		[Display(Name = "姓")]
-		[Required]
 		public string LastName { get; set; }
 
 		public bool Enable { get; set; }
---
+++ Final_Project-7_Client/Team-7_WebApi-Client/Models/Views/RegisterVm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Team_7_WebApi_Client.Models.Views
+{
+    public class RegisterVm
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "帳號")]
+        [Required]
+        [StringLength(30)]
+        public string Account { get; set; }
+
+        [Display(Name = "密碼")]
+        [Required]
+        [StringLength(50)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "確認密碼")]
+        [Required]
+        [StringLength(50)]
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "電子郵件")]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "名")]
+        [Required]
+        [StringLength(10)]
+        public string FirstName { get; set; }
+
+        [Display(Name = "姓")]
+        [Required]
+        [StringLength(10)]
+        public string LastName { get; set; }
+    }
+}
