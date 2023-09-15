f1cf78b3d0ec0e45490546e0d06cef45bfda8c3f
linyang1228 <linyang01228@gmail.com>
2023-09-15 17:24:27 +0800

    add order

---
+++ Final_Project-7_Client/Team-7_WebApi-Client/Models/Views/OrderVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Team_7_WebApi_Client.Models.Views
+{
+	public class OrderVM
+	{
+		public int OrderId { get; set; }
+		public int MemberId { get; set; }
+		public string MemberFirstName { get; set; }
+		public string MemberLastName { get; set; }
+		public string Address { get; set; }
+
+		public int Total { get; set; }
+		public string Status { get; set; }
+		public DateTime OrderTime { get; set; }
+	}
+
+	public class OrderItemVM
+	{
+		public int Id { get; set; }
+		public string ProductName { get; set; }
+		public string Size { get; set; }
+		public int Qty { get; set; }
+		public int Price { get; set; }
+		public int Subtotal { get; set; }
+	}
+}
