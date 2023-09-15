fd18a1216fda76631f7fc8c2931eee02b12d31dd
albert891112 <albert96002891112@gmail.com>
2023-09-15 09:11:43 +0800

    fix prodcutRepository

--- Final_Project-7_Client/Team-7_WebApi-Client/Repositories/ProductRepository.cs
+++ Final_Project-7_Client/Team-7_WebApi-Client/Repositories/ProductRepository.cs
@@ -124,58 +124,58 @@ namespace Team_7_WebApi_Client.Repositories
         /// </summary>
         /// <param name="entity"></param>
         /// <returns></returns>
-        public List<ProductEntity> Search(ProductSearchEntity entity)
-        {
-            string sql = "SELECT p.* , C.* , S.* " +
-                 "FROM Products as P " +
-                 "INNER JOIN " +
-                 "(Categroysies_Products as CP INNER JOIN Categories as C ON CP.CategoryId = C.Id)  " +
-                 "ON P.id = CP.ProductId " +
-                 "INNER JOIN Stock as S ON S.Id = P.StockId " +
-                 "WHERE (@CategoryId IS NUll OR C.Id = @CategoryId) " +
-                 "AND (@Name IS NULL OR P.Name LIKE '%' + @Name +'%') " +
-                 "AND (@Pricelow IS NULL OR P.Price >= @Pricelow) " +
-                 "AND (@Pricehight IS NULL OR P.Price <= @PriceHight) " +
-                 "AND (P.Enable = true)" +
-                 "Order By p.Id";
+        //public List<ProductEntity> Search(ProductSearchEntity entity)
+        //{
+        //    string sql = "SELECT p.* , C.* , S.* " +
+        //         "FROM Products as P " +
+        //         "INNER JOIN " +
+        //         "(Categroysies_Products as CP INNER JOIN Categories as C ON CP.CategoryId = C.Id)  " +
+        //         "ON P.id = CP.ProductId " +
+        //         "INNER JOIN Stock as S ON S.Id = P.StockId " +
+        //         "WHERE (@CategoryId IS NUll OR C.Id = @CategoryId) " +
+        //         "AND (@Name IS NULL OR P.Name LIKE '%' + @Name +'%') " +
+        //         "AND (@Pricelow IS NULL OR P.Price >= @Pricelow) " +
+        //         "AND (@Pricehight IS NULL OR P.Price <= @PriceHight) " +
+        //         "AND (P.Enable = true)" +
+        //         "Order By p.Id";
 
-            object obj = new
-            {
-                categoryId = entity.Categories.Id,
-                name = entity.Name,
-                pricelow = entity.LowPrice,
-                pricehight = entity.HeightPrice,
+        //    object obj = new
+        //    {
+        //        categoryId = entity.Categories.Id,
+        //        name = entity.Name,
+        //        pricelow = entity.LowPrice,
+        //        pricehight = entity.HeightPrice,
 
-            };
+        //    };
 
-            //delegate for Query
-            Func<SqlConnection, string, object, List<ProductEntity>> func = (conn, pro, o) =>
-            {
-                Dictionary<int, ProductEntity> products = new Dictionary<int, ProductEntity>();
-                conn.Query<ProductEntity, CategoryEntity, StockEntity, ProductEntity>(pro, (product, category, stock) =>
-                {
-                    if (!products.TryGetValue(product.Id, out ProductEntity p))
-                    {
-                        product.Stock = stock;
-                        product.Categories = new List<CategoryEntity>();
-                        product.Categories.Add(category);
-                        products.Add(product.Id, product);
-                    }
-                    else
-                    {
-                        p.Categories.Add(category);
-                    }
-                    return p;
+        //    //delegate for Query
+        //    Func<SqlConnection, string, object, List<ProductEntity>> func = (conn, pro, o) =>
+        //    {
+        //        Dictionary<int, ProductEntity> products = new Dictionary<int, ProductEntity>();
+        //        conn.Query<ProductEntity, CategoryEntity, StockEntity, ProductEntity>(pro, (product, category, stock) =>
+        //        {
+        //            if (!products.TryGetValue(product.Id, out ProductEntity p))
+        //            {
+        //                product.Stock = stock;
+        //                product.Categories = new List<CategoryEntity>();
+        //                product.Categories.Add(category);
+        //                products.Add(product.Id, product);
+        //            }
+        //            else
+        //            {
+        //                p.Categories.Add(category);
+        //            }
+        //            return p;
 
-                }, o, commandType: CommandType.StoredProcedure);
+        //        }, o, commandType: CommandType.StoredProcedure);
 
-                return products.Values.ToList();
-            };
+        //        return products.Values.ToList();
+        //    };
 
-            List<ProductEntity> result = connection.Search<List<ProductEntity>>(procedure, "default", obj, func);
+        //    List<ProductEntity> result = connection.Search<List<ProductEntity>>(procedure, "default", obj, func);
 
-            return result;
-        }
+        //    return result;
+        //}
 
     }
 }
