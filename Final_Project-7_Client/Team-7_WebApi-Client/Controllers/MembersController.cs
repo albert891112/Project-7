4d3ce0b72a95a4906958c31e171e3dc668525268
LAPTOP-LOCHRS9S\medam <medamayaki09@gmail.com>
2023-09-16 17:52:43 +0800

    add Register View. Modify layout

---
+++ Final_Project-7_Client/Team-7_WebApi-Client/Controllers/MembersController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Team_7_WebApi_Client.Models.Views;
+
+namespace Team_7_WebApi_Client.Controllers
+{
+    public class MembersController : Controller
+    {
+        // GET: Members
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		public ActionResult Register()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public ActionResult Register(RegisterVm vm)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(vm);
+			}
+			try
+			{
+				//RegisterMember(vm);
+			}
+			catch (Exception ex)
+			{
+				ModelState.AddModelError("", ex.Message);
+				return View(vm);
+			}
+			return View("RegisterConfirm");
+		}
+	}
+}
