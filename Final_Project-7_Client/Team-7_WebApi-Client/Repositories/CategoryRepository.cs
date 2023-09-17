e23802bd8ce41b120bad149b601d91e917dd5f6b
Vincent <eafw0310@gmail.com>
2023-09-17 18:41:43 +0800

    [fix]

---
+++ Final_Project-7_Client/Team-7_WebApi-Client/Repositories/CategoryRepository.cs
@@ -0,0 +1,42 @@
+﻿using Albert.Lib;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+using Team_7_WebApi_Client.Models.Entities;
+using Dapper;
+
+namespace Team_7_WebApi_Client.Repositories
+{
+    public class CategoryRepository
+    {
+        SqlDb connection = new SqlDb();
+
+        //Get Category by Gender 
+        public List<CategoryEntity> Get(int Gender)
+        {
+            string sql = "SELECT C.* FROM GenderCategories as G " +
+                "INNER JOIN GenderCategory_Category as GC ON G.Id = GC.GenderCategoryId " +
+                "INNER JOIN Categories as C ON C.Id = CategoryId " +
+                "WHERE G.Gender = @Gender;";
+
+            object obj = new
+            {
+                Gender = Gender,
+            };
+
+
+            Func<SqlConnection, string, object, List<CategoryEntity>> func = (conn, s, o) =>
+            {
+                return conn.Query<CategoryEntity>(s, o).ToList();
+            };
+
+
+            List<CategoryEntity> entity = this.connection.Get<List<CategoryEntity>>(sql, "default", obj, func);
+
+            return entity;
+        }
+    }
+}
