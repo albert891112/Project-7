e1a5bfd2f549fce46b158541f7f1a0b7de9213d8
LAPTOP-LOCHRS9S\medam <medamayaki09@gmail.com>
2023-09-18 22:56:09 +0800

    add LoginVm, Login.html, modify members controller

---
+++ Final_Project-7_Client/Team-7_WebApi-Client/Models/Views/Members/LoginVm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Team_7_WebApi_Client.Models.Views.Members
+{
+	public class LoginVm
+	{
+        [Display(Name="帳號")]
+        [Required]
+        public string Account { get; set; }
+
+        [Display(Name="密碼")]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="記住我")]
+        public bool RememberMe { get; set; }=false;
+    }
+}
