63e63b21dd7289648bd8616d42f2efda25fe2da2
DESKTOP-5GL18A3\River <medamayaki09@gmail.com>
2023-09-18 14:11:27 +0800

    Modify RegisterVm, Register html

--- Final_Project-7_Client/Team-7_WebApi-Client/Models/Views/Members/RegisterVm.cs
+++ Final_Project-7_Client/Team-7_WebApi-Client/Models/Views/Members/RegisterVm.cs
@@ -12,35 +12,35 @@ namespace Team_7_WebApi_Client.Models.Views
         public int Id { get; set; }
 
         [Display(Name = "帳號")]
-        [Required]
+        [Required(ErrorMessage = "{0}必填")]
         [StringLength(50)]
         public string Account { get; set; }
 
         [Display(Name = "密碼")]
-        [Required]
+        [Required(ErrorMessage = "{0}必填")]
         [StringLength(70)]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Display(Name = "確認密碼")]
-        [Required]
+        [Required(ErrorMessage = "{0}必填")]
         [StringLength(70)]
         [Compare(nameof(Password))]
         [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
 
         [Display(Name = "電子郵件")]
-        [Required]
-        [EmailAddress]
+        [Required(ErrorMessage = "{0}必填")]
+        [EmailAddress(ErrorMessage ="{0}格式錯誤")]
         public string Email { get; set; }
 
-        [Display(Name = "名")]
-        [Required]
+        [Display(Name = "名字")]
+        [Required(ErrorMessage = "{0}必填")]
         [StringLength(50)]
         public string FirstName { get; set; }
 
-        [Display(Name = "姓")]
-        [Required]
+        [Display(Name = "姓氏")]
+        [Required(ErrorMessage = "{0}必填")]
         [StringLength(50)]
         public string LastName { get; set; }
     }
