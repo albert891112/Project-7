c100d57ce3482e98c27480c7d106520b64476d2a
linyang1228 <linyang01228@gmail.com>
2023-09-19 17:12:01 +0800

    Cart.html

---
+++ Final_Project-7_Client/Team-7_WebApi-Client/Controllers/Cart/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Team_7_WebApi_Client.Controllers.Cart
+{
+    public class ApiController : Controller
+    {
+        // GET: Api
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
