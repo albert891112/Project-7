7f76ead6c09f39ef2781f0ecfc93ca02d37858a1
WIN11\vincent <eafw0310@gmail.com>
2023-09-25 16:10:54 +0800

    [feature] Add Order related Entitys

---
+++ Final_Project-7/7-Team_WebApi/Models/Entities/CouponEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _7_Team_WebApi.Models.Entities
+{
+    public class CouponEntity
+    {
+        public int Id { get; set; }
+        public string CouponCode { get; set; }
+        public string CouponName { get; set; }
+        public DiscountTypeEntity Discount { get; set; }
+        public decimal DiscountValue { get; set; }
+        public string CouponDescription { get; set; }
+        public DateTime ExpiratinDate { get; set; }
+        public int UsageCount { get; set; }
+        public bool Enable { get; set; }
+        public string Image { get; set; }
+    }
+
+    public class DiscountTypeEntity
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}
--- Final_Project-7/7-Team_WebApi/Models/Entities/OrderEntity.cs
+++ Final_Project-7/7-Team_WebApi/Models/Entities/OrderEntity.cs
@@ -9,9 +9,14 @@ namespace _7_Team_WebApi.Models.Entities
     {
         public int Id { get; set; }
         public MemberEntity Member { get; set; }
-        public StatusEntity OrderStatus { get; set; }
+        public OrderStatusEntity OrderStatus { get; set; }
+        public string PhoneNumber { get; set; }
         public string Address { get; set; }
+        public CouponEntity Coupon { get; set; }
+        public ShippingEntity Shipping { get; set; }
+        public PaymentEntity Payment { get; set; }
         public DateTime OrderTime { get; set; }
+        public int Total { get; set; }
         public List<OrderItemEntity> OrderItemList { get; set; }
     }
 }
