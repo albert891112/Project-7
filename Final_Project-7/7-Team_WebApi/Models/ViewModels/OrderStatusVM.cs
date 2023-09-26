0d1b87ac7b5fb1d11f31fe8c2b773e4df805f701
DESKTOP-5GL18A3\River <medamayaki09@gmail.com>
2023-09-26 15:48:10 +0800

    add VMs

---
+++ Final_Project-7/7-Team_WebApi/Models/ViewModels/OrderStatusVM.cs
@@ -0,0 +1,27 @@
+﻿using _7_Team_WebApi.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _7_Team_WebApi.Models.ViewModels
+{
+    public class OrderStatusVM
+    {
+        public int Id { get; set; }
+        public string Status { get; set; }
+    }
+
+    public static class OrderStatusExtenssion
+    {
+        public static OrderStatusVM ToVM(this OrderStatusDTO dto)
+        {
+            return new OrderStatusVM()
+            {
+                Id = dto.Id,
+                Status = dto.Status,
+            };
+        }
+    } 
+    
+}
---
+++ Final_Project-7/7-Team_WebApi/Models/ViewModels/ShippingVM.cs
@@ -0,0 +1,28 @@
+﻿using _7_Team_WebApi.Models.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _7_Team_WebApi.Models.ViewModels
+{
+    public class ShippingVM
+    {
+        public int Id { get; set; }
+        public string ShippingMethod { get; set; }
+        public int Price { get; set; }
+    }
+
+    public static class ShippingVMExtenssion
+    {
+        public static ShippingVM ToVM(this ShippingDTO vm)
+        {
+            return new ShippingVM()
+            {
+                Id = vm.Id,
+                ShippingMethod = vm.ShippingMethod,
+                Price = vm.Price,
+            };
+        }
+    }
+}
