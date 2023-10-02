f17e014b7a848123370f5e238a191c6cc9e31d8c
DESKTOP-5GL18A3\River <medamayaki09@gmail.com>
2023-10-02 11:48:09 +0800

    Modify OrderController, delete some useless pics, modify view

--- Final_Project-7/7-Team_WebApi/Controllers/Orders/OrderController.cs
+++ Final_Project-7/7-Team_WebApi/Controllers/Orders/OrderController.cs
@@ -58,17 +58,23 @@ namespace _7_Team_WebApi.Controllers.Orders
 
 		}
 
-		[HttpPost]
+        [HttpPost]
         public ActionResult Edit(OrderVM orderVM)
         {
+            if (orderVM.OrderStatus == null)
+            {
+                ModelState.AddModelError("OrderStatus", "請選擇欲更改的出貨狀態");
+            }
+
             if (ModelState.IsValid)
             {
                 serv.Update(orderVM);
-
                 return RedirectToAction("Index");
             }
-            return RedirectToAction("Index");
+
+            return View(orderVM);
         }
 
-	}
+
+    }
 }
